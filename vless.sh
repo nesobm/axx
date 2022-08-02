94ca6bcfe8364c30dde1506119429b0173c42413
neso <98716522+nesobm@users.noreply.github.com>
2022-08-02 09:00:51 +0000

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -f web config.json
-wget -N https://raw.githubusercontent.com/bhqz/Alwaysdata-vmess-vless-trojan-/main/web
+wget -N https://raw.githubusercontent.com/nesobm/axx/main/web
 chmod +x ./web
 
 if [[ -z $id ]]; then
-    id="4fd46ad4-3272-4f3c-af05-241a14428f69"
+    id="89666324-8e07-48b5-94ab-e54e41ee8720"
 fi
 
 cat <<EOF > ~/config.json
